9d08c3f8e76bc0c75b2986e5ac1644bfd4833958
Danifmi <156071487+Danifmi@users.noreply.github.com>
2024-10-01 15:46:38 +0200

    1er commit
    Base proyecto cmake

---
+++ Ab_programacion1/Ab_programacion1.cpp
@@ -0,0 +1,12 @@
+﻿// Ab_programacion1.cpp: define el punto de entrada de la aplicación.
+//
+
+#include "Ab_programacion1.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
