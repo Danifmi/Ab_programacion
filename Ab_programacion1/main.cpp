7ec890dbd8d77410d1f1e401fdf6a28f56fededb
Danifmi <156071487+Danifmi@users.noreply.github.com>
2024-11-25 17:28:51 +0100

    Organización estructura
    He creado diferentes carpetas para contener las clases diferenciando entre encabezados (include/)  y fuentes (src/).
    Además se ha cambiado el nombre del archivo principal a main.cpp.

---
+++ Ab_programacion1/main.cpp
@@ -0,0 +1,11 @@
+﻿// Ab_programacion1.cpp: define el punto de entrada de la aplicación.
+//
+
+#include "Ab_programacion1.h"
+
+
+int main()
+{
+	std::cout << "Hello CMake." << std::endl;
+	return 0;
+}
